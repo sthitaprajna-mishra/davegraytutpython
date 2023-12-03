9a0c3fb9500cde3178ed2763cb6f6811b0f0dfb5
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-12-03 20:19:35 +0530

    first commit

---
+++ lesson3/meaning.py
@@ -0,0 +1,10 @@
+meaning = 3
+print("")
+
+# if meaning > 10:
+#     print("Right on!")
+# else:
+#     print("Whatever")
+
+# Ternary operator
+print("Right on!") if meaning > 10 else print("Not today")
